fa1b1500d19b6d7463eb56cf441bae80679c9ebb
XeroDN <rananiroj69@gmail.com>
2025-06-08 07:57:36 +0545

    update

---
+++ Function/positional_argument.py
@@ -0,0 +1,9 @@
+def describe_type(animal_type, pet_name):
+    """ displaying a pet type"""
+    print(f"\nI have a reptile {animal_type.title()}")    
+    print(f"My {animal_type} name is {pet_name.title()}")  
+describe_type("snake",'hola')  
+# calling function multiple times
+
+describe_type('crocodile','crybaby')
+describe_type('camelon','colorchanger')
