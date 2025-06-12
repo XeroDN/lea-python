b761f4fcbb68fa04403af8cae19cd86e5850fa10
XeroDN <rananiroj69@gmail.com>
2025-06-12 22:00:32 +0545

    update

--- Function/returning_dictionary.py
+++ Function/returning_dictionary.py
@@ -13,3 +13,11 @@ def build_person(first_name,last_name, age=()):
     return person
 physicts = build_person('Niroj','Rana',45)
 print(physicts)
+
+def build_person(first_name,last_name,age=None):
+    person = {'first':first_name,'last':last_name}
+    if age:
+        person['age'] = age
+    return person
+physicts = build_person('Niroj','Rana',34)
+print(physicts)
