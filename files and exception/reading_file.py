0fd902274b2139dae44e1eb96ed195ef174737cd
XeroDN <rananiroj69@gmail.com>
2025-06-18 17:05:46 +0545

    initial commit

--- files and exception/reading_file.py
+++ files and exception/reading_file.py
@@ -4,3 +4,29 @@ with open(file_path) as file_object:
        one argument: the name of the file you want to open."""
     content = file_object.read() # read function read the content from the pi.txt file
 print(content.lstrip())
+
+# reading line by line 
+file_path = 'pi.txt'
+with open(file_path) as file_object:
+    for line in file_object: # read function read the content from the pi.txt file
+        print(line.strip())
+
+# making list of line from file
+file_path = 'C:/Users/xero__t94rj10/OneDrive/Desktop/learn/python/pyth/files and exception/pi.txt'
+with open(file_path) as file_object:
+    lines = file_object.readlines()
+for line in lines:
+    print(line.strip())
+
+
+# working with file content
+file_path = 'C:/Users/xero__t94rj10/OneDrive/Desktop/learn/python/pyth/files and exception/pi.txt'
+with open(file_path) as file_object:
+    lines = file_object.readlines()
+
+pi = ''
+for line in lines:
+    pi += line.rstrip()
+
+print(pi)
+print(len(pi))
