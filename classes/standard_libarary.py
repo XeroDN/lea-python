f814a724c7970ec6239af7745a0f613f38e18b0c
XeroDN <rananiroj69@gmail.com>
2025-06-18 11:52:48 +0545

    commit

---
+++ classes/standard_libarary.py
@@ -0,0 +1,7 @@
+from random import randint
+print(randint(2,100))
+
+from random import choice
+name = ['niroj','rana','sujan','hari','nirmal','kapil']
+first_choices = choice(name)
+print(first_choices)
